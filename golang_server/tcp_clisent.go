236218c9c0746889440dc476a0f79be90c0f3451
Georgik0 <egor.udin.ru@gmail.com>
2021-11-08 13:50:27 +0300

    add: tcp client server on golang

---
+++ golang_server/tcp_clisent.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
+
+	defer conn.Close()
+	reader_stdin := bufio.NewReader(os.Stdin)
+
+
+	for {
+		go func() {
+			fmt.Printf("Input text to send: ")
+			text, _ := reader_stdin.ReadString('\n')
+			fmt.Fprintf(conn, text)
+		}()
+
+		answer, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Printf("Answer server: %v", answer)
+	}
+}
